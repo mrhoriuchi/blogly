b1667cbc134ab1a7f276f7b277d4dd4fbb59d13f
Kody Horiuchi <kody.a.horiuchi@gmail.com>
2022-12-20 12:47:28 -0500

    added image for users and able to return full name

--- models.py
+++ models.py
@@ -4,6 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+DEFAULT_IMAGE = "https://www.freeiconspng.com/uploads/icon-user-blue-symbol-people-person=generic--public-domain--21.png"
+
 
 def connect_db(app):
     """Connect to database."""
@@ -20,3 +22,10 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     first_name = db.Column(db.Text, nullable=False)
     last_name = db.Column(db.Text, nullable=False)
+    image_url = db.Column(db.Text, nullable=False, default=DEFAULT_IMAGE)
+
+    @property
+    def full_name(self):
+        """Returns the full name of the user"""
+
+        return f"{self.first_name} {self.last_name}"
