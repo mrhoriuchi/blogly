3dbc4ced2330267fe13ab99f13634650a81a30c6
Kody Horiuchi <kody.a.horiuchi@gmail.com>
2022-12-20 12:26:12 -0500

    added some tempates

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 """Blogly application."""
 
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, request, redirect, render_template
+from models import db, connect_db, User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
@@ -10,3 +10,18 @@ app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
 db.create_all()
+
+
+@app.route('/')
+def homepage():
+    """Redirects to list of users"""
+
+    return redirect()
+
+
+@app.route('/users')
+def users_index():
+    """Shows a page with info on all users"""
+
+    users = User.query.order_by(User.last_name, User.first_name).all()
+    return render_template('users/index.html', users=users)
